a6cbfc8fd0404a7afda1e2a1d602523b3db06a7d
Sandro Gehri <sandrogehri@gmail.com>
2023-08-09 06:38:49 +0200

    remove unnecessary test

--- tests/Feature/ExtendTailwindMergeTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-test('extendTailwindMerge works correctly with single config')->todo();
-test('extendTailwindMerge works correctly with multiple configs')->todo();
-test('extendTailwindMerge works correctly with function config')->todo();
