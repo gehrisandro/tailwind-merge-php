554392f358231e0892f3c5d07148cc9ca5e43617
Sandro Gehri <sandrogehri@gmail.com>
2023-08-09 07:04:51 +0200

    implement custom separator tests

--- tests/Feature/SeparatorsTest.php
+++ tests/Feature/SeparatorsTest.php
@@ -2,36 +2,34 @@
 
 use TailwindMerge\TailwindMerge;
 
-test('single character separator working correctly', function () {
-    //    const twMerge = extendTailwindMerge({
-    //        separator: '_',
-    //    })
+test('single character separator working correctly', function ($input, $output) {
+    $instance = TailwindMerge::factory()
+        ->withConfiguration([
+            'separator' => '_',
+        ])
+        ->make();
 
-    expect(TailwindMerge::instance()->merge('block hidden'))->toBe('hidden');
+    expect($instance->merge($input))->toBe($output);
+})->with([
+    ['block hidden', 'hidden'],
+    ['p-3 p-2', 'p-2'],
+    ['!right-0 !inset-0', '!inset-0'],
+    ['hover_focus_!right-0 focus_hover_!inset-0', 'focus_hover_!inset-0'],
+    ['hover:focus:!right-0 focus:hover:!inset-0', 'hover:focus:!right-0 focus:hover:!inset-0'],
+]);
 
-    expect(TailwindMerge::instance()->merge('p-3 p-2'))->toBe('p-2');
+test('multiple character separator working correctly', function ($input, $output) {
+    $instance = TailwindMerge::factory()
+        ->withConfiguration([
+            'separator' => '__',
+        ])
+        ->make();
 
-    expect(TailwindMerge::instance()->merge('!right-0 !inset-0'))->toBe('!inset-0');
-
-    expect(TailwindMerge::instance()->merge('hover_focus_!right-0 focus_hover_!inset-0'))->toBe('focus_hover_!inset-0');
-    expect(TailwindMerge::instance()->merge('hover:focus:!right-0 focus:hover:!inset-0'))->toBe(
-        'hover:focus:!right-0 focus:hover:!inset-0',
-    );
-})->todo();
-
-test('multiple character separator working correctly', function () {
-    //    const twMerge = extendTailwindMerge({
-    //        separator: '__',
-    //    })
-
-    expect(TailwindMerge::instance()->merge('block hidden'))->toBe('hidden');
-
-    expect(TailwindMerge::instance()->merge('p-3 p-2'))->toBe('p-2');
-
-    expect(TailwindMerge::instance()->merge('!right-0 !inset-0'))->toBe('!inset-0');
-
-    expect(TailwindMerge::instance()->merge('hover__focus__!right-0 focus__hover__!inset-0'))->toBe('focus__hover__!inset-0');
-    expect(TailwindMerge::instance()->merge('hover:focus:!right-0 focus:hover:!inset-0'))->toBe(
-        'hover:focus:!right-0 focus:hover:!inset-0',
-    );
-})->todo();
+    expect($instance->merge($input))->toBe($output);
+})->with([
+    ['block hidden', 'hidden'],
+    ['p-3 p-2', 'p-2'],
+    ['!right-0 !inset-0', '!inset-0'],
+    ['hover__focus__!right-0 focus__hover__!inset-0', 'focus__hover__!inset-0'],
+    ['hover:focus:!right-0 focus:hover:!inset-0', 'hover:focus:!right-0 focus:hover:!inset-0'],
+]);
