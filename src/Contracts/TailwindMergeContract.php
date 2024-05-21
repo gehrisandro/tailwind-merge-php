bbd7ae3ac307b9736de39de4c3a989bc96eb413a
Chetan <contact@mrchetan.com>
2024-05-21 22:40:53 +0530

    Update TailwindMergeContract.php (#10)

--- src/Contracts/TailwindMergeContract.php
+++ src/Contracts/TailwindMergeContract.php
@@ -5,7 +5,7 @@ namespace TailwindMerge\Contracts;
 interface TailwindMergeContract
 {
     /**
-     * @param  array<array-key, string|array<array-key, string>>  ...$args
+     * @param  string|array<array-key, string|array<array-key, string>>  ...$args
      */
     public function merge(...$args): string;
 }
